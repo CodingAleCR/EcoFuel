18955e7580a3c3ffa83f374febf2bb696e093395
CodingAleCR <alejandroulate@gmail.com>
2017-11-27 09:52:50 -0600

    kajj

--- app/src/main/java/info/codingalecr/ecofuel/models/MFuelItem.java
+++ app/src/main/java/info/codingalecr/ecofuel/models/MFuelItem.java
@@ -54,6 +54,6 @@ public class MFuelItem {
 
     @Override
     public String toString() {
-        return "Kms" + getKilometers() + " / " + getAmountCash();
+        return "Kms" + getKilometers() + " / Cash: " + getAmountCash() + " / Liters: " + getAmountLt();
     }
 }
