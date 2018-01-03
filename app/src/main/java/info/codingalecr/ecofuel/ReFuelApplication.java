3223b8630838d0e001b0a2f91be9e73204acc269
aulate <aulate@soin.co.cr>
2018-01-03 10:16:11 -0600

    Updating gradle and android studio support

---
+++ app/src/main/java/info/codingalecr/ecofuel/ReFuelApplication.java
@@ -0,0 +1,31 @@
+package info.codingalecr.ecofuel;
+
+import android.app.Application;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+
+import info.codingalecr.ecofuel.views.activities.FuelListActivity;
+
+/**
+ * Created by aulate on 12/12/17.
+ */
+
+public class ReFuelApplication extends Application {
+
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+
+
+
+
+}
